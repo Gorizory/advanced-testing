1d405569e42ec9729afa3dcb40795a32d4f65164
Gorizory <sobenin.g@gmail.com>
2021-05-25 11:57:32 +0300

    Add layouts service

---
+++ common/libs/env.ts
@@ -0,0 +1,24 @@
+let env: any = {
+    NODE_ENV: 'development',
+    ...process.env,
+};
+
+// passed env from frontback to client
+if (typeof window !== 'undefined') {
+    Object.assign(env, (window as any)?.env);
+}
+
+env = {
+    ...env,
+    IS_DEVELOPMENT: env.NODE_ENV === 'development',
+};
+
+export const {
+    APP_ENV,
+    IS_DEVELOPMENT,
+    NODE_ENV,
+    NODE_PATH,
+    PROJECT_ROOT,
+} = env;
+
+export default env;
